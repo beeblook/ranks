15b9cf7328935ee985c3975e8c960769419de00c
Keisuke Nemoto <nemoto@colorchips.co.jp>
2013-02-02 03:34:20 +0900

    かさばる見た目を調整

--- classes/views/setting/index.php
+++ classes/views/setting/index.php
@@ -14,68 +14,61 @@
 			<div class="ranks-box">
 				<div class="ranks-box-label"><?php echo $pattern['label']; ?></div>
 				<div class="ranks-box-columns">
-					<dl class="ranks-datalist">
-						<dt><span>パターンキー</span></dt>
-						<dd><?php echo $pattern_key; ?></dd>
-						<dt><span>レート</span></dt>
-						<dd>
+					<div class="ranks-header">
+						<?php echo $pattern_key; ?>
+					</div>
+					<dd class="ranks-ratedata">
 <?php foreach($pattern['rates'] as $account_slug => $rate) : if ($rate == 0 || !isset($accounts[$account_slug]) || !$accounts[$account_slug]['status']) continue; ?>
-							<div class="ranks-rates-input <?php echo $account_slug; ?>">
-								<span class="ranks-rates-label"><?php echo $accounts[$account_slug]['label']; ?></span>
-								× <?php echo $rate; ?>
-							</div>
+						<div class="ranks-rates-input <?php echo $account_slug; ?>">
+							<span class="ranks-rates-label"><?php echo $accounts[$account_slug]['label']; ?></span>
+							× <?php echo $rate; ?>
+						</div>
 <?php endforeach; ?>
-						</dd>
-					</dl>
-					<dl class="ranks-datalist">
-						<dt><span>投稿タイプ</span></dt>
-						<dd><?php
+					</dd>
+					<div class="ranks-posttypedata">
+						<?php
 							$types = array();
 							foreach ($pattern['post_type'] as $post_type) {
 								$post_type_object = get_post_type_object($post_type);
-								$types[] = $post_type_object->label . ' <span class="description">(' . $post_type_object->name . ')</span>';
+								$types[] = $post_type_object->label;// . ' <span class="description">(' . $post_type_object->name . ')</span>';
 							}
 							echo join('<br>', $types);
-						?></dd>
-	<!--
+						?>
+					</div>
+					<dl class="ranks-datalist">
 						<dt><span>表示件数</span></dt>
 						<dd><?php echo number_format_i18n($pattern['posts_per_page']); ?>位まで表示</dd>
 	 					<dt><span>集計期間</span></dt>
 						<dd><?php
 							$unit = array_shift(array_keys($pattern['term']));
 							$n = $pattern['term'][$unit];
-							echo sprintf($terms[$unit], $n) . ' <span class="description">(' . date('Y年n月j日', strtotime("$n $unit ago")) . ' から ' . date('Y年n月j日') . ')</span>';
+							echo sprintf($terms[$unit], $n);// . '<br><span class="description">(' . date('Y年n月j日', strtotime("$n $unit ago")) . ' から ' . date('Y年n月j日') . ')</span>';
 						?></dd>
+<?php if (!empty($pattern['schedule_event'])) : ?>
 						<dt><span>自動集計</span></dt>
 						<dd><?php
-							if (empty($pattern['schedule_event'])) {
-								echo '<span class="description">(使用しない)</span>';
-							} else {
-								switch ($pattern['schedule_event']['type']) {
-									case 'daily':
-										echo '毎日 ';
-										break;
-									case 'weekly':
-										echo '毎週 ' . $wp_locale->get_weekday($pattern['schedule_event']['week']);
-										break;
-									case 'monthly':
-										echo '毎月 ' .  $pattern['schedule_event']['day'] . '日';
-										break;
-								}
-								echo ' ' . $pattern['schedule_event']['hour'] . '時に実行';
-								echo ' <span class="description">(次回予定: ' . date_i18n('Y年n月j日 G時', $pattern['next_schedule'] + (get_option('gmt_offset') * 3600)) . ')</span>';
+							switch ($pattern['schedule_event']['type']) {
+								case 'daily':
+									echo '毎日 ';
+									break;
+								case 'weekly':
+									echo '毎週 ' . $wp_locale->get_weekday($pattern['schedule_event']['week']);
+									break;
+								case 'monthly':
+									echo '毎月 ' .  $pattern['schedule_event']['day'] . '日';
+									break;
 							}
+							echo ' ' . $pattern['schedule_event']['hour'] . '時に実行';
+							echo ' <span class="description">(次回予定: ' . date_i18n('Y年n月j日 G時', $pattern['next_schedule'] + (get_option('gmt_offset') * 3600)) . ')</span>';
 						?></dd>
+<?php endif; ?>
+<?php if (!empty($pattern['rewrite_rule'])) : ?>
 						<dt><span>ランキングページ</span></dt>
 						<dd><?php
-							if (empty($pattern['rewrite_rule'])) {
-								echo '<span class="description">(使用しない)</span>';
-							} else {
-								$url = home_url($pattern['rewrite_rule']);
-								echo '<a href="' . $url . '" target="_blank">' . $url . '</a>';
-							}
+							$url = home_url($pattern['rewrite_rule']);
+							echo '<a href="' . $url . '" target="_blank">' . $url . '</a>';
 						?></dd>
-	 -->
+<?php endif; ?>
 					</dl>
 				</div>
 				<div class="ranks-action">
@@ -97,24 +90,26 @@
 <?php foreach ($accounts as $account_slug => $account) : ?>
 			<div class="ranks-box <?php echo $account_slug; if (!$account['status']) echo ' invalid'; ?>">
 				<div class="ranks-box-label"><?php echo $account['label']; ?></div>
-				<dl class="ranks-datalist">
-					<dt><span>ステータス</span></dt>
-					<dd><?php echo $account['status'] ? '有効' : '無効'; ?></dd>
+				<div class="ranks-box-columns">
+					<dl class="ranks-datalist">
+						<dt><span>ステータス</span></dt>
+						<dd><?php echo $account['status'] ? '有効' : '無効'; ?></dd>
 <?php if (isset($account['profile_id'])) : ?>
-					<dt><span>プロファイル</span></dt>
-					<dd>
-						<?php echo $account['profile_id'] ? 'ga:'.$account['profile_id'] : ''; ?>
-					</dd>
+						<dt><span>プロファイル</span></dt>
+						<dd>
+							<?php echo $account['profile_id'] ? 'ga:'.$account['profile_id'] : ''; ?>
+						</dd>
 <?php endif; ?>
 <?php if (isset($account['term'])) : ?>
-					<dt><span>データ取得期間</span></dt>
-					<dd><?php
-						$unit = array_shift(array_keys($account['term']));
-						$n = $account['term'][$unit];
-						echo sprintf($terms[$unit], $n) . ' <span class="description">(' . date('Y年n月j日', strtotime("$n $unit ago")) . ' から ' . date('Y年n月j日') . ')</span>';
-					?></dd>
+						<dt><span>データ取得期間</span></dt>
+						<dd><?php
+							$unit = array_shift(array_keys($account['term']));
+							$n = $account['term'][$unit];
+							echo sprintf($terms[$unit], $n) . ' <span class="description">(' . date('Y年n月j日', strtotime("$n $unit ago")) . ' から ' . date('Y年n月j日') . ')</span>';
+						?></dd>
 <?php endif; ?>
-				</dl>
+					</dl>
+				</div>
 				<div class="ranks-action">
 <?php if ($account['status']) : ?>
 					<a href="<?php echo $this->url('account_count', array('account'=>$account_slug)); ?>" class="ranks-action-button">データ更新</a>
